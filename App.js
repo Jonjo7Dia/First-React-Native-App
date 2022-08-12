2ad2d2733f95a7181f5b972b245b8160bea113a9
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-12 19:04:23 -0300

    implementing state

--- App.js
+++ App.js
@@ -1,44 +1,57 @@
-
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [listOfGoals, setListOfGoals] = useState([]);
+  function goalInputHandler(e) {
+    setEnteredGoalText(e);
+  }
+
+  function addGoalHandler() {
+    setListOfGoals((prevState) => [...prevState, enteredGoalText]);
+
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder ="Your course goal!" style={styles.textInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Your course goal!"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text> List of goals...</Text>
+        {listOfGoals.map((goal,index) =>  <Text key={index}>{goal}</Text>)}
       </View>
     </View>
-  ); 
+  );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
-  inputContainer :{
+  inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccc'
+    borderBottomColor: "#cccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#ccccc',
-    width: '70%',
+    borderColor: "#ccccc",
+    width: "70%",
     marginRight: 8,
-    padding: 8
-
+    padding: 8,
   },
   goalsContainer: {
     flex: 3,
-  }
+  },
 });
